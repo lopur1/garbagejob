2ae4c9daa2daa5771bcd1b703d5b2c9803eb434c
lopur1 <89532873+lopur1@users.noreply.github.com>
2025-02-26 17:03:20 +0200

    kaikki

---
+++ t_garbage/fxmanifest.lua
@@ -0,0 +1,20 @@
+
+fx_version 'cerulean'
+
+game 'gta5'
+author 'tulppu.'
+description 'Wow hullu roskaduuni projekti'
+lua54 'yes'
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+    'dumpsters.lua'
+} 
+
+client_scripts{
+    'client/client.lua',
+}
+
+server_scripts{
+    'server/server.lua',
+}
